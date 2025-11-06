1b703a7372e649283ae3823e0024be2665f7aec4
thiziri2024 <Thiziri.aziz@fgei.ummto.dz>
2025-11-06 23:58:38 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,33 @@
-const express = require('express');     // 1 création du serveur complet avc javascript
-const app = express()                   // 2 à l'aide de express qui rend la création (ces 2 lignes) tres simple 
+const express = require("express");
 const http = require("http");
-const cors = require("cors"); 
-const {Server} = require("socket.io");
-const { Socket } = require('dgram');
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
 app.use(cors());
 
-const server = http.createServer(app);      //3eme ligne de la creation du serveur avc node et la bibliotheque express
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
-        methods: ["GET","POST"],
+        methods: ["GET", "POST"],
     },
 });
 
-io.on("connection", () => {    //le serveur écoute les événements : “message envoyé”
-    console.log("user disconnected", socket.id);     
+// Quand un client se connecte :
+io.on("connection", (socket) => {
+    console.log("Un utilisateur est connecté :", socket.id);
 
-    Socket.on("disconnect", () => {           //le client envoie des événements : “message reçu”
-        console.log("user disconnected", socket.id);
+    socket.on("disconnect", () => {
+    console.log("Un utilisateur s'est déconnecté :", socket.id);
     });
-});     //Ce code dit : dès qu’un utilisateur envoie un message, tout le monde connecté reçoit le message en direct.
+});
 
+app.get("/", (req, res) => {
+    res.send("Le serveur Socket.io fonctionne !");
+});
 
-server.listen(3001, ()=> {
-    console.log("server running");
+server.listen(3001, () => {
+    console.log("Serveur en écoute sur le port 3001");
 });
