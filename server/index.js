932e253b7745576c67694b04915122f464898fbb
thiziri2024 <Thiziri.aziz@fgei.ummto.dz>
2025-11-07 18:56:13 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,20 @@
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: { origin: "*" } // permet toutes les connexions pour tester
 });
 
-// Quand un client se connecte :
-io.on("connection", (socket) => {
-    console.log("Un utilisateur est connecté :", socket.id);
+io.on('connection', (socket) => {
+    console.log('Un client est connecté !');
 
-    socket.on("disconnect", () => {
-    console.log("Un utilisateur s'est déconnecté :", socket.id);
+    socket.on('chat message', (msg) => {
+        console.log('Message reçu :', msg);
+        io.emit('chat message', msg); // renvoie à tous
     });
 });
 
-app.get("/", (req, res) => {
-    res.send("Le serveur Socket.io fonctionne !");
-});
-
-server.listen(3001, () => {
-    console.log("Serveur en écoute sur le port 3001");
-});
+server.listen(3000, () => console.log("Serveur lancé sur le port 3000"));
